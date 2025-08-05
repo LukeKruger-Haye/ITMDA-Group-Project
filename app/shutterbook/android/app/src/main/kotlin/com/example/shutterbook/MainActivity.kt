772d1069dedee2fecd60382f15e956f4fe6ad770
Luke Kruger-Haye <eduv4777435@vossie.net>
2025-08-05 10:23:00 +0200

    flutter init

---
+++ app/shutterbook/android/app/src/main/kotlin/com/example/shutterbook/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shutterbook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app/shutterbook/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Shutterbook',
+            theme: ThemeData(
+                primarySwatch: Colors.blue,
+            ),
+            home: MyHomePage(),
+        );
+    }
+}
+
+class MyHomePage extends StatefulWidget {
+    @override
+    _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+    int _currentIndex = 0;
+
+    final List<Widget> _tabs = [
+        HomeScreen(),
+        BookingsScreen(),
+        ManageScreen(),
+    ];
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Shutterbook'),
+            ),
+            body: _tabs[_currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+                currentIndex: _currentIndex,
+                onTap: (int index) {
+                    setState(() {
+                        _currentIndex = index;
+                    });
+                },
+                items: [
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        label: 'Home',
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.favorite),
+                        label: 'Bookings',
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.person),
+                        label: 'Manage',
+                    ),
+                ],
+            ),
+        );
+    }
+}
+
+class HomeScreen extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Center(
+            child: Text('Home Screen'),
+        );
+    }
+}
+
+class BookingsScreen extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Center(
+            child: Text('Bookings Screen'),
+        );
+    }
+}
+
+class ManageScreen extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Center(
+            child: Text('Manage Screen'),
+        );
+    }
+}
