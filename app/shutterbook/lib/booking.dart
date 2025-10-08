3feb34946b220bf46a7e7a3120c019d12b2a3e6d
Luke Kruger-Haye <eduv4777435@vossie.net>
2025-10-08 14:46:18 +0200

    db init

---
+++ app/shutterbook/lib/booking.dart
@@ -0,0 +1,35 @@
+class Booking {
+    int? bookingId;
+    int clientId;
+    DateTime bookingDate;
+    String status;
+    DateTime? createdAt;
+
+    Booking({
+        this.bookingId,
+        required this.clientId,
+        required this.bookingDate,
+        this.status = 'Scheduled',
+        this.createdAt,
+    });
+
+    Map<String, dynamic> toMap() {
+        return {
+            'booking_id': bookingId,
+            'client_id': clientId,
+            'booking_date': bookingDate.toString(),
+            'status': status,
+            'created_at': createdAt?.toString(),
+        };
+    }
+
+    factory Booking.fromMap(Map<String, dynamic> map) {
+        return Booking(
+            bookingId: map['booking_id'],
+            clientId: map['client_id'],
+            bookingDate: DateTime.parse(map['booking_date']),
+            status: map['status'] ?? 'Scheduled',
+            createdAt: map['created_at'] != null ? DateTime.tryParse(map['created_at']) : null,
+        );
+    }
+}
---
+++ app/shutterbook/lib/quote.dart
@@ -0,0 +1,34 @@
+class Quote {
+    int? id;
+    int clientId;
+    double totalPrice;
+    String description;
+    DateTime? createdAt;
+
+    Quote({
+        this.id,
+        required this.clientId,
+        required this.totalPrice,
+        required this.description,
+        this.createdAt,
+    });
+
+    Map<String, dynamic> toMap() {
+        return {
+            'id': id,
+            'client_id': clientId,
+            'total_price': totalPrice,
+            'description': description,
+        };
+    }
+
+    factory Quote.fromMap(Map<String, dynamic> map) {
+        return Quote(
+            id: map['id'],
+            clientId: map['client_id'],
+            totalPrice: map['total_price'],
+            description: map['description'],
+            createdAt: map['created_at'] != null ? DateTime.tryParse(map['created_at']) : null
+        );
+    }
+}
