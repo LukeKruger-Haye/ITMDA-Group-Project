ddda21becf081eb608c60e32f6189ef98a9229cb
Luke Kruger-Haye <eduv4777435@vossie.net>
2025-10-10 11:02:44 +0200

    linking mostly done

--- app/shutterbook/lib/main.dart
+++ app/shutterbook/lib/main.dart
@@ -1,26 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:shutterbook/pages/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:shutterbook/pages/authentication/setup.dart';
+import 'package:shutterbook/pages/authentication/login.dart';
 
 import 'package:shutterbook/pages/bookings/bookings.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ShutterBookApp());
 }
 
-class ShutterBookApp extends StatelessWidget {
+class ShutterBookApp extends StatefulWidget {
   const ShutterBookApp({super.key});
 
   @override
+  State<ShutterBookApp> createState() => _ShutterBookAppState();
+}
+
+class _ShutterBookAppState extends State<ShutterBookApp> {
+  Widget? _startScreen;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkPasswordStatus();
+  }
+
+  Future<void> _checkPasswordStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedPassword = prefs.getString('app_password');
+
+    setState(() {
+      if (savedPassword == null || savedPassword.isEmpty) {
+        _startScreen = const SetupScreen();
+      } else {
+        _startScreen = const LoginScreen();
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (_startScreen == null) {
+      return const MaterialApp(
+        home: Scaffold(
+          body: Center(child: CircularProgressIndicator()),
+        ),
+      );
+    }
+
     return MaterialApp(
       title: 'ShutterBook',
       theme: ThemeData(primarySwatch: Colors.indigo),
-      initialRoute: '/',
+      home: _startScreen,
       routes: {
-        '/': (context) => const HomePage(),
-        // '/clients': (context) => const ClientsPage(),
+        '/home': (context) => const HomePage(),
         '/bookings': (context) => const BookingsPage(),
-        // '/quotes': (context) => const QuotesPage(),
       },
     );
   }
