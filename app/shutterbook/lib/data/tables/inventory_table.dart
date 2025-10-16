51b2a2e7bf044e9d10a843d18d9ae17473443928
Luke Kruger-Haye <eduv4777435@vossie.net>
2025-10-16 09:35:04 +0200

    Spelling fix in inventory_table.dart

--- app/shutterbook/lib/data/tables/inventory_table.dart
+++ app/shutterbook/lib/data/tables/inventory_table.dart
@@ -8,7 +8,7 @@ class InventoryTable {
   Future<int> insertItem(Item item) async {
     Database db = await dbHelper.database;
     return await db.insert(
-      'Invtory', 
+      'Inventory', 
       item.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace 
     );
@@ -17,7 +17,7 @@ class InventoryTable {
   Future<Item?> getItemById(int id) async {
     Database db = await dbHelper.database;
     final maps = await db.query(
-      'Invtory', 
+      'Inventory', 
       where: 'item_id = ?',
       whereArgs: [id]
     );
@@ -31,7 +31,7 @@ class InventoryTable {
 
   Future<List<Item>> getAllItems() async {
     Database db = await dbHelper.database;
-    final maps = await db.query('Invtory');
+    final maps = await db.query('Inventory');
     return maps.map((m) => Item.fromMap(m)).toList();
   }
 
@@ -49,7 +49,7 @@ class InventoryTable {
   Future<int> updateItem(Item item) async {
     final db = await dbHelper.database;
     return await db.update(
-      'Invtory',
+      'Inventory',
       item.toMap(),
       where: 'item_id = ?',
       whereArgs: [item.id]
@@ -59,7 +59,7 @@ class InventoryTable {
   Future<int> deleteItem(int id) async {
     final db = await dbHelper.database;
     return await db.delete(
-      'Invtory',
+      'Inventory',
       where: 'item_id = ?',
       whereArgs: [id]
     );
